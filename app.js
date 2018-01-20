778a1c293869cba3d7b208cb3a11d2aae7722959
Rosie Lara <roselara1@gmail.com>
2018-01-20 15:37:48 -0800

    problem 1 passed

--- app.js
+++ app.js
@@ -6,11 +6,18 @@
 	"The sum of 4 and 7 is 11."
 	Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 	// Write your code here
-	function sum(a, b) {
-	//eslint-disable-line
+
+	function sum(a,b) {
+		var add = (a + b);
+		var mes = ('The sum of ' + a + ' and ' + b + ' is ' + add + '.');
+		return [add, mes]
 	}
+
+	// function sum(a, b) {
+	//eslint-disable-line
+	// }
 	// Here is the test for sum(); uncomment it to run it
-	// testSum(4, 7);
+	testSum(4, 7);
 	// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 	/////////////////////////////////////
 	/* Problem 2
