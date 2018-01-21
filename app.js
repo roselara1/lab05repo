516be4b5c16905c09b1b923e3da80df4f0ee4da5
Rosie Lara <roselara1@gmail.com>
2018-01-20 19:14:06 -0800

    fixed errors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 "use strict";	
 	/////////////////////////////////////
 	/* Problem 1 (this is your demo that we'll solve in class)
@@ -6,11 +5,18 @@
 	"The sum of 4 and 7 is 11."
 	Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 	// Write your code here
-	function sum(a, b) {
-	//eslint-disable-line
+
+	function sum(a,b) {
+		var add = (a + b);
+		var mes = ('The sum of ' + a + ' and ' + b + ' is ' + add + '.');
+		return [add, mes]
 	}
+
+	// function sum(a, b) {
+	//eslint-disable-line
+	// }
 	// Here is the test for sum(); uncomment it to run it
-	// testSum(4, 7);
+	testSum(4, 7);
 	// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 	/////////////////////////////////////
 	/* Problem 2
@@ -65,4 +71,3 @@
 	// Here is the test for multiplyArray(); uncomment it to run it
 	// testMultiplyArray(testArray);
 	// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
-
