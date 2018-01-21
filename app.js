14cfa2611ae0cd7caba3ccbe53c407f18bae6b4e
Rosie Lara <roselara1@gmail.com>
2018-01-20 19:21:07 -0800

    problem2 solved

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 
 	function sum(a,b) {
 		var add = (a + b);
-		var mes = ('The sum of ' + a + ' and ' + b + ' is ' + add + '.');
-		return [add, mes]
+		var mes1 = ('The sum of ' + a + ' and ' + b + ' is ' + add + '.');
+		return [add, mes1]
 	}
 
 	// function sum(a, b) {
@@ -24,11 +24,17 @@
 	"The product of 5 and 9 is 45."
 	Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 	// Write your code here
-	function multiply(a, b) {
-	//eslint-disable-line
+    function multiply (a,b){
+		var multiplied = (a * b);
+		var mes2 = ('The product of ' + a + ' and ' + b + ' is ' + multiplied + '.');
+		return [multiplied, mes2];
 	}
+
+	// function multiply(a, b) {
+	//eslint-disable-line
+	// }
 	// Here is the test for multiply(); uncomment it to run it
-	// testMultiply(5,9);
+	testMultiply(5,9);
 	// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 	/////////////////////////////////////
 	/* Problem 3
